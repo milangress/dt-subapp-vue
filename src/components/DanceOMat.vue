9c2cc7c333c20343c932bc4133b933e3f6c9e58e
A. Koch <dasantonym@users.noreply.github.com>
2018-03-28 18:47:19 +0200

    add vuex api backend

--- src/components/DanceOMat.vue
+++ src/components/DanceOMat.vue
@@ -1,14 +1,50 @@
 <template lang="pug">
-  svg(:width="svgSize.width", :height="svgSize.height")
-    g
-      circle(:cx="circlePos.x", :cy="circlePos.y", r="20")
+  div
+    svg(:width="svgSize.width", :height="svgSize.height")
+      g
+        circle(:cx="circlePos.x", :cy="circlePos.y", r="20")
+    div#timelines(v-if="loggedIn")
+      | We have {{ timelines.length }} timelines.
+      button(:click="listTimelines") Fetch Timelines
+    div#loginbox(v-if="!loggedIn")
+      input(type="text", v-model="payload.email", placeholder="email")
+      input(type="password", v-model="payload.password", placeholder="password")
+      button(v-on:click="loginUser") Login
 </template>
 
 <script>
 export default {
   data () {
     return {
-      radius: 200
+      radius: 200,
+      payload: {
+        strategy: 'local',
+        username: undefined,
+        password: undefined
+      },
+      loggedIn: false,
+      timelines: []
+    }
+  },
+  methods: {
+    loginUser () {
+      const _this = this
+      return this.$store.dispatch('auth/authenticate', _this.payload)
+        .then(() => {
+          _this.loggedIn = true
+        })
+        .catch(err => {
+          _this.loggedIn = false
+          console.log(err.message, err)
+        })
+    },
+    listTimelines () {
+      if (!this.loggedIn) return
+      const _this = this
+      this.$store.dispatch('maps/find', { query: { type: 'Timeline' } })
+        .then(timelines => {
+          _this.timelines = timelines
+        })
     }
   },
   computed: {
@@ -39,6 +75,11 @@ export default {
 </script>
 
 <style scoped>
+#loginbox, #timelines {
+  position: absolute;
+  left: 20px;
+  top: 20px;
+}
 circle {
   fill: #999;
   stroke: #333;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import feathersVuex from 'feathers-vuex'
+import { createClient } from './api'
 
 Vue.use(Vuex)
 
+const idField = 'uuid'
+const opts = { idField }
+const client = createClient('https://motionbank-api.herokuapp.com')
+const { service, auth } = feathersVuex(client, opts)
+
 const store = new Vuex.Store({
+  plugins: [
+    service('annotations', opts),
+    service('maps', opts),
+    service('users', opts),
+    auth({ userService: 'users' })
+  ],
   state: {
     time: 0
   },
