6586dbfa79eaadcf54afcc9703d7ac9c5e036488
0idri0 <david.rittershaus@gmail.com>
2018-04-10 18:56:04 +0200

    individual dash-offset

--- src/components/PathDrawer.vue
+++ src/components/PathDrawer.vue
@@ -8,14 +8,14 @@
               :style="{'stroke': selectedColor}",
               :points="path", fill="none", stroke-linejoin="round")
       polyline(v-for="(d, i) in pathCollection", :class="{drawing: true}",
-       :style="{'stroke': usedColors[i], 'stroke-dasharray': pathLength[i], 'stroke-dashoffset': animator}",
+       :style="{'stroke': usedColors[i], 'stroke-dasharray': pathLength[i], 'stroke-dashoffset': animations[i] ? animator[i] : 0}",
        :points="pathCollection[i]", fill="none", stroke-linejoin="round")
 
      g
       circle(cx="50", cy="20%", r="20", stroke="grey", stroke-width="2" fill="white", @click="play")
+      circle(cx="50", cy="30%", r="20", stroke="grey", stroke-width="2" fill="white", @click="stop")
       circle(v-for="(d, i) in colors", cx="50", :cy="circlesY(i)", r="20", stroke="grey", stroke-width="2", :fill="d",
               @click="selectColor(i)")
-    p {{animator}}
 </template>
 
 <script>
@@ -31,17 +31,18 @@ export default {
       colors: ['#83AE9B', '#C8C8A9', '#F9CDAE', '#F69A9A', '#EF4566'],
       selectedColor: '#000000',
       usedColors: [],
-      pathCollection: []
+      pathCollection: [],
+      dashOffset: [],
+      animations: []
     }
   },
 
   computed: {
     animator () {
-      let test
-      this.pathLength.forEach(el => {
-        test = el - (this.$store.state.time * 0.1) % el
-      })
-      return test
+      return this.animate ? this.dashOffset.map((el) => {
+        el = el - (this.$store.state.time * 0.1) % el
+        return el
+      }) : 0
     },
     svgSize () {
       return {
@@ -50,6 +51,15 @@ export default {
       }
     }
   },
+  /* watch: {
+    animator: function (animprocess) {
+      let index
+      index = animprocess.findIndex(el => el < 5)
+      if (index >= 0) {
+        this.animations[index] = false
+      }
+    }
+  }, */
   methods: {
 
     initDraw () {
@@ -67,6 +77,8 @@ export default {
     },
     stopDraw () {
       this.pathLength.push(this.$el.querySelector('.drawing').getTotalLength())
+      this.dashOffset.push(this.$el.querySelector('.drawing').getTotalLength())
+      this.animations.push(true)
       this.pathCollection.push(this.path)
       this.usedColors.push(this.selectedColor)
       this.path = ''
@@ -74,6 +86,9 @@ export default {
     },
     play () {
       this.animate = true
+    },
+    stop () {
+      this.animate = false
     },
     circlesY (i) {
       return this.svgSize.height / 2 * 0.85 + i * 50
