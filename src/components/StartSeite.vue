c0317df14142a88034366060ad559fbfe5f5c411
Florian Jenett <fjenett@gmail.com>
2018-05-02 14:50:32 +0200

    basic index for start page

--- src/components/StartSeite.vue
+++ src/components/StartSeite.vue
@@ -1,13 +1,44 @@
-<template>
-
+<template lang="pug">
+  div#tools
+    router-link(:to="{path: tool.path}").tool(v-for="tool in tools")
+      h1 {{tool.name}}
 </template>
 
 <script>
   export default {
-    name: 'StartSeite'
+    name: 'StartSeite',
+    data () {
+      return {
+        tools: [
+          {
+            name: 'Mr. Griddle',
+            path: '/mr-griddle'
+          },
+          {
+            name: 'Rezepte',
+            path: '/lost-in-space'
+          },
+          {
+            name: 'Lost in Space',
+            path: '/lost-in-space'
+          },
+          {
+            name: 'Video Tool',
+            path: '/lost-in-space'
+          }
+        ]
+      }
+    }
   }
 </script>
 
-<style scoped>
+<style scoped lang="stylus">
+  #tools
+    display flex
+    flex-direction row
+    flex-wrap wrap
 
+  .tool
+    flex-grow 1
+    min-width 200px
 </style>
